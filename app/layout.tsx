499b4748e5d4582382dd64b3bb80aeb8977cbb2b
ebfri <ebfrigon@gmail.com>
2023-01-05 00:25:06 -0800

    Added a grid layout to the main page

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,19 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <html className={`text-gray-300/95 bg-background h-max lg:w-screen lg:text-2xl`}>
-      <body>{children}</body>
-    </html>
-  )
+    return (
+        <html className={`text-gray-300/95 h-max w-screen`}>
+        <body>
+        <div className="fixed inset-0 flex justify-center sm:px-8 bg-black">
+            <div className="flex w-full max-w-7xl lg:px-8">
+                <div className="w-full ring-1 bg-zinc-900 ring-zinc-300/20" />
+            </div>
+        </div>
+        <div className="relative">
+            {children}
+        </div>
+        </body>
+
+        </html>
+    )
 }
