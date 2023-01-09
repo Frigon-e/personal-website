b401e87612403cc10fb19cd250f92ae1b81314da
ebfri <ebfrigon@gmail.com>
2023-01-08 16:38:48 -0800

    Removed Projects from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import Image, {StaticImageData} from 'next/image'
-import TicTacToe from "../components/TicTacToe";
-import {v4 as uuid} from "uuid";
+import Image from 'next/image'
 import {Container} from '../components/Container';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import KRCLogo from '../components/KRCLogo'
@@ -106,7 +104,7 @@ const Lang_Grid = () => {
 
 const Tech_Grid = () => {
     return (
-        <div className="divide-y divide-gray-200 overflow-hidden sm:rounded-lg bg-inherit shadow grid grid-cols-3 border-t border-grey-200 sm:border-none sm:pt-0 sm:gap-1 divide-y-0">
+        <div className="divide-y divide-gray-200 overflow-hidden sm:rounded-lg shadow grid grid-cols-3 border-t border-grey-200 sm:border-none sm:pt-0 sm:gap-1 divide-y-0">
             {Techs.map((tech, techID) => (
                 <div
                     key={tech.name}
@@ -131,35 +129,6 @@ const Tech_Grid = () => {
     )
 }
 
-function GameDescription() {
-    const headers = ['How to win', 'How to play'];
-    const descriptions = [['Win by getting 3 in a row in any of the 9 3x3 grids'],
-        [['Click on a cell to place your piece.'], ['The next move must be in the grid that corresponds to the cell you clicked on.'], ['If the corresponding board is complete you can place on any board']]];
-
-    const listItems = headers.map((header, index) => {
-        return (
-            <div key={uuid()} className={`flex flex-col space-y-2`}>
-                <h3 key={uuid()} className={`mt-2 text-base bg-gradient-to-tr from-pink-500 to-rose-500 bg-clip-text text-transparent font-bold`}>{header}</h3>
-                {descriptions[index].map((description) => {
-                    return (
-                        <li key={uuid()} className={`text-sm`}>{description}</li>
-                    )
-                })}
-            </div>
-        )
-    });
-
-    return (
-        <>
-            <h1 key={uuid()} className={`mt-4 pb-2 text-2xl sm:text-3xl bg-gradient-to-tr from-emerald-400 to-cyan-500 bg-clip-text text-transparent font-bold`}>Projects</h1>
-            <div key={uuid()} className={`mb-6 rounded-2xl border p-6 border-zinc-700/40`}>
-                <h2 key={uuid()} className={`text-xl bg-gradient-to-tr from-emerald-400 to-cyan-500 bg-clip-text text-transparent font-bold`}>Ultimate TicTacToe</h2>
-                {listItems}
-            </div>
-        </>
-    );
-}
-
 interface BriefcaseIconProps {
     className: string;
 }
@@ -177,13 +146,98 @@ function BriefcaseIcon(props: BriefcaseIconProps) {
         >
             <path
                 d="M2.75 9.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z"
-                className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"
+                className="fill-zinc-100/10 stroke-zinc-500"
             />
             <path
                 d="M3 14.25h6.249c.484 0 .952-.002 1.316.319l.777.682a.996.996 0 0 0 1.316 0l.777-.682c.364-.32.832-.319 1.316-.319H21M8.75 6.5V4.75a2 2 0 0 1 2-2h2.5a2 2 0 0 1 2 2V6.5"
-                className="stroke-zinc-400 dark:stroke-zinc-500"
+                className="stroke-zinc-500"
             />
         </svg>
+    )
+}
+
+function CertHistory() {
+    const resume: {
+        cert_name: string;
+        cert_company: string;
+        logo: string | typeof KRCLogo;
+        issue: string | { label: string; dateTime: number };
+        expire: string | { label: string; dateTime: number };
+    }[] = [
+        {
+            cert_name: 'SFA/CPR-C [OFA1] ',
+            cert_company: 'Lifesaving Society',
+            logo: '/static/logo/lifesavingsocietyIcon.png',
+            issue: "Oct 2022",
+            expire: "Oct 2025"
+        },
+        {
+            cert_name: 'National Lifeguard Pool',
+            cert_company: 'Lifesaving Society',
+            logo: '/static/logo/lifesavingsocietyIcon.png',
+            issue: 'Jun 2022',
+            expire: 'Jun 2024',
+        },
+        {
+            cert_name: 'National Lifeguard Waterpark',
+            cert_company: 'Lifesaving Society',
+            logo: '/static/logo/lifesavingsocietyIcon.png',
+            issue: 'May 2021',
+            expire: 'May 2023',
+        },
+        {
+            cert_name: 'RCA Coach',
+            cert_company: 'Rowing Canada',
+            logo: '/static/logo/RCAIcon.png',
+            issue: 'Aug 2019',
+            expire: 'Lifetime',
+        },
+
+    ]
+    return (
+        <div className="mt-6 rounded-2xl border p-6 border-zinc-700/40">
+            <h2 className="flex text-sm font-semibold text-zinc-100">
+                <BriefcaseIcon className="h-6 w-6 flex-none"/>
+                <span className="ml-3">Certs</span>
+            </h2>
+            <ol className="mt-6 space-y-4">
+                {resume.map((role, roleIndex) => (
+                    <li key={roleIndex} className="flex gap-4">
+                        <div
+                            className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-zinc-900/5 border border-zinc-700/50 bg-zinc-800 ring-0">
+                            {typeof role.logo === 'string' ? <Image src={role.logo} alt="" width={1000} height={1000} className="h-7 w-7"/> : <KRCLogo/>}
+
+                        </div>
+                        <dl className="flex flex-auto flex-wrap gap-x-2">
+                            <dt className="sr-only">Cert Name</dt>
+                            <dd className="w-full flex-none text-sm font-medium text-zinc-100">
+                                {role.cert_name}
+                            </dd>
+                            <dt className="sr-only">Cert Company</dt>
+                            <dd className="text-xs text-zinc-400">
+                                {role.cert_company}
+                            </dd>
+                            <dt className="sr-only">Date</dt>
+                            <dd
+                                className="ml-auto text-xs text-zinc-500"
+                                aria-label={`${typeof role.issue !== "string" ? role.issue.label : role.issue} until ${
+                                    typeof role.expire !== "string" ? role.expire.label : role.expire
+                                }`}
+                            >
+                                <time dateTime={typeof role.issue !== "string" ? String(role.issue.dateTime) : role.issue}>
+                                    {typeof role.issue !== "string" ? role.issue.label : role.issue}
+                                </time>
+                                {' '}
+                                <span aria-hidden="true">—</span>{' '}
+                                <time dateTime={typeof role.expire !== "string" ? String(role.expire.dateTime) : role.expire}>
+                                    {typeof role.expire !== "string" ? role.expire.label : role.expire}
+                                </time>
+                            </dd>
+                        </dl>
+                    </li>
+                ))}
+            </ol>
+        </div>
     )
 }
 
@@ -228,30 +282,31 @@ function WorkHistory() {
         },
     ]
     return (
-        <div className="mt-6 rounded-2xl bg-black/30 border border-zinc-100 p-6 dark:border-zinc-700/40">
-            <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
+        <div className="mt-6 rounded-2xl border p-6 border-zinc-700/40">
+            <h2 className="flex text-sm font-semibold text-zinc-100">
                 <BriefcaseIcon className="h-6 w-6 flex-none"/>
                 <span className="ml-3">Work</span>
             </h2>
             <ol className="mt-6 space-y-4">
                 {resume.map((role, roleIndex) => (
                     <li key={roleIndex} className="flex gap-4">
-                        <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-zinc-900/5 border border-zinc-700/50 bg-zinc-800 ring-0">
-                            {typeof role.logo ==='string' ? <Image src={role.logo} alt="" width={0} height={0}  className="h-7 w-7"/> : <KRCLogo/> }
+                        <div
+                            className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-zinc-900/5 border border-zinc-700/50 bg-zinc-800 ring-0">
+                            {typeof role.logo === 'string' ? <Image src={role.logo} alt="" width={1000} height={1000} className="h-7 w-7"/> : <KRCLogo/>}
 
                         </div>
                         <dl className="flex flex-auto flex-wrap gap-x-2">
                             <dt className="sr-only">Company</dt>
-                            <dd className="w-full flex-none text-sm font-medium text-zinc-900 dark:text-zinc-100">
+                            <dd className="w-full flex-none text-sm font-medium text-zinc-100">
                                 {role.company}
                             </dd>
                             <dt className="sr-only">Role</dt>
-                            <dd className="text-xs text-zinc-500 dark:text-zinc-400">
+                            <dd className="text-xs text-zinc-400">
                                 {role.title}
                             </dd>
                             <dt className="sr-only">Date</dt>
                             <dd
-                                className="ml-auto text-xs text-zinc-400 dark:text-zinc-500"
+                                className="ml-auto text-xs text-zinc-500"
                                 aria-label={`${typeof role.start !== "string" ? role.start.label : role.start} until ${
                                     typeof role.end !== "string" ? role.end.label : role.end
                                 }`}
@@ -261,7 +316,7 @@ function WorkHistory() {
                                 </time>
                                 {' '}
                                 <span aria-hidden="true">—</span>{' '}
-                                <time dateTime={typeof role.end !=="string" ? String(role.end.dateTime) : role.end}>
+                                <time dateTime={typeof role.end !== "string" ? String(role.end.dateTime) : role.end}>
                                     {typeof role.end !== "string" ? role.end.label : role.end}
                                 </time>
                             </dd>
@@ -277,9 +332,9 @@ function WorkHistory() {
 export default function Home() {
     return (
         <>
-            <Container>
+            <Container className={`mt-16 sm:t-32`}>
                 <div className={`max-w-2xl`}>
-                    <h1 className={`text-4xl font-bold tracking-tight text-zinc-100 sm:text-5xl`}>Student, Developer and Employable</h1>
+                    <h1 className={`text-4xl font-bold tracking-tight text-zinc-100 sm:text-5xl`}>Student, Lifeguard, Developer and Employable</h1>
 
                     <p className={`mt-6 text-base text-zinc-400`}>Hello, my name is
                         <span className={`bg-gradient-to-tr from-emerald-400 to-cyan-500 bg-clip-text text-transparent font-bold`}> Ethan Frigon</span>
@@ -288,9 +343,11 @@ export default function Home() {
                 </div>
                 <Lang_Grid/>
                 <Tech_Grid/>
-                <WorkHistory/>
-                <GameDescription key={uuid()}/>
-                <TicTacToe key={uuid()}/>
+                <div className={`md:grid md:grid-cols-2 md:gap-1`}>
+                    <WorkHistory/>
+                    <CertHistory/>
+                </div>
+
             </Container>
         </>
     )
