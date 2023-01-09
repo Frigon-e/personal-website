9d5fa794451bba0b10af9062dc332f5107b24f46
ebfri <ebfrigon@gmail.com>
2023-01-08 16:35:33 -0800

    Added Nav bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,24 @@
 import '../styles/globals.css';
+import Header from "../components/Header";
+import {Container} from "../components/Container";
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
     return (
-        <html className={`text-gray-300/95 h-max w-screen`}>
+        <html className={`text-gray-300/95`}>
         <body>
         <div className="fixed inset-0 flex justify-center sm:px-8 bg-black">
             <div className="flex w-full max-w-7xl lg:px-8">
                 <div className="w-full ring-1 bg-zinc-900 ring-zinc-300/20" />
             </div>
         </div>
+
         <div className="relative">
+            <Container>
+                <Header/>
+            </Container>
             {children}
         </div>
         </body>
