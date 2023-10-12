1bf2558159c3b8eef72c5182409071d5470387b5
smurfssmur <ebfrigon@gmail.com>
2023-10-11 19:44:13 -0700

    Move Files around

---
+++ frontend/src/components/TicTacToe.tsx
@@ -0,0 +1,181 @@
+'use client';
+
+import { useEffect, useState } from "react";
+
+type Moves = { Row: number, Col: number }
+
+export default function TicTacToe() {
+  const [currentPlayer, setCurrentPlayer] = useState('X');
+  const [gameboard, setGameboard] = useState<(string | null)[][][]>(Array(9).fill(Array(3).fill(Array(3).fill(null))));
+  const [playerMoves, setPlayerMoves] = useState<Moves[]>([] as Moves[]);
+  const [computerMoves, setComputerMoves] = useState<Moves[]>([] as Moves[]);
+  const [lastCMPMove, setLastCMPMove] = useState({ Board: -1, Row: -1, Col: -1 });
+  const [finishedBoards, setFinishedBoards] = useState(Array(3).fill(Array(3).fill(0)));
+  const [validMoves, setValidMoves] = useState(Array(3).fill(Array(3).fill(true)));
+  const [gameOver, setGameOver] = useState(false);
+  const [winner, setWinner] = useState(null);
+  const [isComputer, setIsComputer] = useState(false);
+
+  useEffect(() => {
+    // console.log("effect used to set computer");
+
+    if (isComputer && currentPlayer === 'O' && !gameOver) {
+      makeComputerMove();
+    }
+  }, [currentPlayer, isComputer]);
+
+  const handleCellClick = (board: number, row: number, col: number) => {
+    const boardThreeByThree = getRowColFlattenBoard(board)
+    if (gameboard[board][row][col] != null || gameOver || currentPlayer === 'O' || !validMoves[Math.floor(boardThreeByThree.Row)][Math.floor(boardThreeByThree.Col)]) {
+      return;
+    }
+
+    let newGameboard = JSON.parse(JSON.stringify(gameboard));
+    newGameboard[board][row][col] = currentPlayer;
+    setGameboard(newGameboard);
+
+    const newMove = convertToEightByEight(board, row, col);
+    let newPlayerMoves = playerMoves;
+    newPlayerMoves.push(newMove);
+    setPlayerMoves(newPlayerMoves);
+
+
+    setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
+    setIsComputer(!isComputer);
+
+  }
+
+  const makeComputerMove = () => {
+    // Make a post request to the server to get the computer's move
+    console.log(JSON.stringify({ "ComputerMoves": computerMoves, "PlayerMoves": playerMoves, "FinishedBoards": finishedBoards, "ValidMoves": validMoves }));
+    fetch('https://api.frigon-e.ca/api/move/', { body: JSON.stringify({ "ComputerMoves": computerMoves, "PlayerMoves": playerMoves }), method: 'POST' })
+      .then(response =>
+        response.json())
+      .then(data => {
+        console.log("Data received from server");
+        console.log(data);
+
+        let newComputerMoves: Moves[] = data.computerMoves.map((move: Moves) => ({
+          Row: move.Row,
+          Col: move.Col
+        }));
+
+        setComputerMoves(newComputerMoves);
+
+        setGameOver(data.finishedGame.gameOver);
+        setWinner(data.finishedGame.winner);
+
+        if (data.winner === 'X') {
+          return;
+        }
+
+
+        const computerLastMove = convertToThreeByThree(newComputerMoves[newComputerMoves.length - 1].Row, newComputerMoves[newComputerMoves.length - 1].Col);
+        setLastCMPMove(computerLastMove);
+        const playerLastMove = convertToThreeByThree(playerMoves[playerMoves.length - 1].Row, playerMoves[playerMoves.length - 1].Col);
+
+        let newGameboard = JSON.parse(JSON.stringify(gameboard));
+        newGameboard[computerLastMove.Board][computerLastMove.Row][computerLastMove.Col] = 'O';
+        setGameboard(newGameboard);
+
+        let newFinishedBoards = JSON.parse(JSON.stringify(data.finishedGame.completedBoard));
+
+        setFinishedBoards(newFinishedBoards);
+
+        let newValidMoves = JSON.parse(JSON.stringify(data.validMoves));
+        setValidMoves(newValidMoves);
+
+
+        if (data.finishedGame.gameOver) {
+          return;
+        }
+
+        setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
+        setIsComputer(!isComputer);
+
+        // console.log("Variables updated");
+        // console.log("Computer moves: " + JSON.stringify(newComputerMoves));
+        // console.log("Finished boards: " + JSON.stringify(newFinishedBoards));
+        // console.log("Game over: " + gameOver);
+        // console.log("Winner: " + winner);
+      });
+
+  }
+
+  const convertToEightByEight = (board: number, row: number, col: number) => {
+    const newRow = row + (3 * Math.floor(board / 3));
+    const newCol = col + (3 * (board % 3));
+    return { Row: newRow, Col: newCol };
+  }
+
+  const convertToThreeByThree = (row: number, col: number) => {
+    const newBoard = Math.floor(row / 3) * 3 + Math.floor(col / 3);
+    const newRow = row % 3;
+    const newCol = col % 3;
+    return { Board: newBoard, Row: newRow, Col: newCol };
+  }
+
+  const getRowColFlattenBoard = (num: number) => {
+    const newRow = num / 3
+    const newCol = num % 3
+
+    return { Row: newRow, Col: newCol }
+  }
+
+  function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ')
+  }
+
+  return (
+    <div>
+      {gameOver && (
+        <div>
+          {winner ? (
+            <p> {winner === 2 ? "Human " : "Computer "} wins!</p>
+          ) : (
+            <p> Game is a draw </p>
+          )}
+          <button onClick={() => window.location.reload()} className={`mb-3`}>Play Again</button>
+        </div>
+      )}
+      <div className={`max-w-fit max-h-fit mx-auto mb-8`}>
+        {/* Show a message if the computer is making a move */}
+        {isComputer && currentPlayer === 'O' && !gameOver && <p>The computer is making a move...</p>}
+        <div className="grid grid-cols-3 gap-2 bg-gradient-to-tr from-emerald-400 to-cyan-500">
+          {gameboard.map((board, boardIndex) => (
+            finishedBoards[Math.floor(boardIndex / 3)][Math.floor(boardIndex % 3)] === 0 ? (
+              <div key={boardIndex} className={classNames(
+                validMoves[Math.floor(boardIndex / 3)][Math.floor(boardIndex % 3)] && !gameOver ? `bg-gradient-to-r from-pink-500 to-rose-500` : `bg-zinc-700`,
+                `grid grid-flow-row gap-1 bg-zinc-900`)}>
+
+                {board.map((row, rowIndex) => (
+                  <div key={rowIndex} className={`grid grid-flow-col gap-1`}>
+                    {row.map((col, colIndex) => (
+                      <div
+                        key={colIndex}
+                        onClick={() => {
+                          handleCellClick(boardIndex, rowIndex, colIndex)
+                        }}
+                        className={classNames(
+                          lastCMPMove.Board != -1 ? (lastCMPMove.Board === boardIndex && lastCMPMove.Row === rowIndex && lastCMPMove.Col === colIndex) ? `border-amber-200/40 border` : `` : ``,
+                          `flex h-8 w-8 text-center items-center justify-center bg-zinc-900/95 sm:w-12 sm:h-12 lg:w-16 lg:h-16`)}>
+                        {col || ' '}
+                      </div>
+                    ))}
+                  </div>
+                ))}
+              </div>
+            ) : (
+              finishedBoards[Math.floor(boardIndex / 3)][Math.floor(boardIndex % 3)] === 1 ? (
+                <div className={`flex bg-zinc-900 justify-center items-center text-center aspect-square text-8xl sm:text-9xl`}> O </div>
+              ) : (
+                <div className={`flex bg-zinc-900 justify-center items-center text-center aspect-square text-8xl sm:text-9xl`}> X </div>
+              )
+
+            )
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ server/golang/helperFuncs.go
@@ -0,0 +1,454 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func tripleEqual(a int, b int, c int) bool {
+	return a == b && b == c && a != 0
+}
+
+func tripleEqualWithZero(a int, b int, c int) bool {
+	return a == b && b == c
+}
+
+func twoEqualToNum(a int, b int, c int, num int) bool {
+	if ((a == b) || (a == c)) && a == num {
+		return true
+	}
+	if b == c && c == num {
+		return true
+	}
+	return false
+}
+
+func oneFromWin(a int, b int, c int, num int) bool {
+	if a == b && a != c && c == 0 && a == num {
+		return true
+	}
+	if a == c && a != b && b == 0 && a == num {
+		return true
+	}
+	if b == c && b != a && a == 0 && b == num {
+		return true
+	}
+	return false
+}
+
+// returns the indices in order that would sort an array.
+// for example, if the array is [3,2,1] then the returned array will be [1,2,3]
+func getSortedIndices(miniBoard [3][3]int) []int {
+	flattenedEvalBoard := getFlattenedBoard(&miniBoard)
+	var indices []int
+	for i := 0; i < len(flattenedEvalBoard); i++ {
+		indices = append(indices, i)
+	}
+	sort.Slice(indices, func(i, j int) bool {
+		return flattenedEvalBoard[indices[i]] > flattenedEvalBoard[indices[j]]
+	})
+	return indices
+}
+
+func isMovesLeftRunner(board [9][3][3]int) bool {
+	numberOfJobs := len(board)
+	jobs := make(chan [3][3]int, numberOfJobs)
+	results := make(chan bool, numberOfJobs)
+
+	for workerNum := 0; workerNum < numberOfJobs/2; workerNum++ {
+		go isMovesLeftWorker(jobs, results)
+	}
+
+	for index := 0; index < numberOfJobs; index++ {
+		jobs <- board[index]
+	}
+	close(jobs)
+
+	for numberOfResults := 0; numberOfResults < numberOfJobs; numberOfResults++ {
+		fmt.Println(<-results)
+	}
+
+	return false
+}
+
+func isMovesLeftWorker(jobs <-chan [3][3]int, results chan<- bool) {
+	for currentJob := range jobs {
+		results <- isMovesLeft(currentJob)
+	}
+}
+
+func isMovesLeft(miniBoard [3][3]int) bool {
+	for rowIndex := 0; rowIndex < len(miniBoard); rowIndex++ {
+		for colIndex := 0; colIndex < len(miniBoard[rowIndex]); colIndex++ {
+			if miniBoard[rowIndex][colIndex] == 0 {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func makeMainBoard(playerMoves []move, computerMoves []move) [9][3][3]int {
+	var newBoard [9][3][3]int
+	for miniBoardNum := 0; miniBoardNum < 9; miniBoardNum++ {
+		for rowIndex := 0; rowIndex < 3; rowIndex++ {
+			for colIndex := 0; colIndex < 3; colIndex++ {
+				newBoard[miniBoardNum][rowIndex][colIndex] = 0
+			}
+		}
+	}
+	for _, player := range playerMoves {
+		threeByThreeLoc := convertToThreeByThree(player.Row, player.Col)
+		newBoard[threeByThreeLoc[0]][threeByThreeLoc[1]][threeByThreeLoc[2]] = 2
+	}
+	for _, computer := range computerMoves {
+		threeByThreeLoc := convertToThreeByThree(computer.Row, computer.Col)
+		newBoard[threeByThreeLoc[0]][threeByThreeLoc[1]][threeByThreeLoc[2]] = 1
+	}
+	return newBoard
+}
+
+func makeFinishedBoard(board [9][3][3]int) [3][3]int {
+	var newBoard [3][3]int
+	for rowIndex := 0; rowIndex < 3; rowIndex++ {
+		for colIndex := 0; colIndex < 3; colIndex++ {
+			value := checkSingleBoardWinner(board[colIndex+(rowIndex*3)], 0)
+			switch {
+			case value > 0:
+				newBoard[rowIndex][colIndex] = 1
+			case value < 0:
+				newBoard[rowIndex][colIndex] = 2
+			default:
+				newBoard[rowIndex][colIndex] = 0
+			}
+		}
+	}
+	return newBoard
+}
+
+func makeEvaluationBoard() {
+	for boardNum := 0; boardNum < 9; boardNum++ {
+		locationOfBoard := getRowColFlattenBoard(boardNum)
+		if finishedBoard[locationOfBoard[0]][locationOfBoard[1]] == 0 {
+			evaluateBoard(&mainBoard[boardNum], boardNum, &evaluationBoard)
+		}
+	}
+}
+
+func printMainBoard(board *[9][3][3]int) {
+	for allRows := 0; allRows < 3; allRows++ {
+		for colIndex := 0; colIndex < 3; colIndex++ {
+			for miniBoardCol := 0; miniBoardCol < 3; miniBoardCol++ {
+				// currentMiniBoard := float64(allRows/3) * 3
+				print(board[miniBoardCol+allRows*3][colIndex][0], " ")
+				print(board[miniBoardCol+allRows*3][colIndex][1], " ")
+				print(board[miniBoardCol+allRows*3][colIndex][2], "  ")
+			}
+			println()
+		}
+		println()
+	}
+}
+
+func printMiniBoard(board *[3][3]int) {
+	for rowIndex := 0; rowIndex < 3; rowIndex++ {
+		for colIndex := 0; colIndex < 3; colIndex++ {
+			print(board[rowIndex][colIndex], " ")
+		}
+		println()
+	}
+}
+
+func getRowColFlattenBoard(num int) [2]int {
+	newRow := num / 3
+	newCol := num - (newRow * 3)
+
+	return [2]int{newRow, newCol}
+}
+
+func getValidBoards(currentRow int, currentCol int, finishedBoard [3][3]int) [3][3]bool {
+	validBoards := [3][3]bool{{false, false, false}, {false, false, false}, {false, false, false}}
+	newRow := currentRow % 3
+	newCol := currentCol % 3
+	if finishedBoard[newRow][newCol] == 0 {
+		validBoards[newRow][newCol] = true
+		return validBoards
+	}
+
+	for rowIndex := 0; rowIndex < 3; rowIndex++ {
+		for colIndex := 0; colIndex < 3; colIndex++ {
+			if finishedBoard[rowIndex][colIndex] == 0 {
+				validBoards[rowIndex][colIndex] = true
+			}
+		}
+	}
+	return validBoards
+}
+
+func isNextBoard(currentRow int, currentCol int, isEightByEight bool) int {
+	if isEightByEight {
+		return isNextBoard(currentRow%3, currentCol%3, false)
+	}
+	return (currentRow * 3) + currentCol
+}
+
+func getNextBoard(row int, col int, maxingPlayer bool) int {
+	nextBoardNumber := isNextBoard(row, col, true)
+	eval := getRowColFlattenBoard(nextBoardNumber)
+	if finishedBoard[eval[0]][eval[1]] != 0 {
+		nextBoardNumber = getBestBoardAny(maxingPlayer)
+	}
+	return nextBoardNumber
+}
+
+func getBestBoardAny(maxingPlayer bool) int {
+	sortedList := getSortedIndices(evaluationBoard)
+	if maxingPlayer {
+		for _, index := range sortedList {
+			bestLoc := getRowColFlattenBoard(index)
+			if finishedBoard[bestLoc[0]][bestLoc[1]] == 0 {
+				return index
+			}
+		}
+	} else {
+		// reverse sortedList
+		for i := len(sortedList) - 1; i >= 0; i-- {
+			bestLoc := getRowColFlattenBoard(sortedList[i])
+			if finishedBoard[bestLoc[0]][bestLoc[1]] == 0 {
+				return sortedList[i]
+			}
+		}
+	}
+	return -1
+}
+
+func getFlattenedBoard(evaluationBoard *[3][3]int) []int {
+	flattenedEvalBoard := make([]int, 0)
+	for row := 0; row < 3; row++ {
+		for col := 0; col < 3; col++ {
+			flattenedEvalBoard = append(flattenedEvalBoard, evaluationBoard[row][col])
+		}
+	}
+	return flattenedEvalBoard
+}
+
+func getCurrentBoard(currentRow int, currentCol int) int {
+	newRow := (currentRow / 3) * 3
+	newCol := currentCol / 3
+	return newRow + newCol
+}
+
+func convertToEightByEight(currentBoard int, currentRow int, currentCol int) [2]int {
+	newRow := ((currentBoard / 3) * 3) + currentRow
+	newCol := ((currentBoard % 3) * 3) + currentCol
+	return [2]int{newRow, newCol}
+}
+
+func convertToThreeByThree(currentRow int, currentCol int) [3]int {
+	newBoard := getCurrentBoard(currentRow, currentCol)
+	newRow := currentRow % 3
+	newCol := currentCol % 3
+	return [3]int{newBoard, newRow, newCol}
+}
+
+func checkSingleBoardWinner(board [3][3]int, depth int) int {
+	results := make(chan int, 3)
+	// var wg sync.WaitGroup
+	// wg.Add(1)
+	go func() {
+		for index := 0; index < 3; index++ {
+			if tripleEqual(board[index][0], board[index][1], board[index][2]) {
+				if board[index][0] == 1 {
+					results <- 25 - depth*3
+					return
+				}
+				if board[index][0] == 2 {
+					results <- -25 + depth*3
+					return
+				}
+			}
+		}
+		results <- 0
+	}()
+
+	go func() {
+		for index := 0; index < 3; index++ {
+			if tripleEqual(board[0][index], board[1][index], board[2][index]) {
+				if board[0][index] == 1 {
+					results <- 25 - depth*3
+					return
+				}
+				if board[0][index] == 2 {
+					results <- -25 + depth*3
+					return
+				}
+			}
+		}
+		results <- 0
+	}()
+
+	go func() {
+		if tripleEqual(board[0][0], board[1][1], board[2][2]) {
+			if board[0][0] == 1 {
+				results <- 25 - depth*3
+				return
+			}
+			if board[0][0] == 2 {
+				results <- -25 + depth*3
+				return
+			}
+		}
+		if tripleEqual(board[0][2], board[1][1], board[2][0]) {
+			if board[0][2] == 1 {
+				results <- 25 - depth*3
+				return
+			}
+			if board[0][2] == 2 {
+				results <- -25 + depth*3
+				return
+			}
+		}
+
+		results <- 0
+	}()
+
+	total := 0
+	for values := 0; values < 3; values++ {
+		total += <-results
+	}
+
+	return total
+}
+
+/*
+:return: returns tile + eval of board
+
+Rather than an individual tile takes the whole board and gives a eval on whole board
+*/
+func evaluateBoard(board *[3][3]int, boardNum int, choiceEvaluationBoard *[3][3]int) {
+	results := make(chan int, 3)
+	go func() {
+		sum := 0
+		for index := 0; index < 3; index++ {
+			if oneFromWin(board[index][0], board[index][1], board[index][2], 1) {
+				sum += 3
+			} else if oneFromWin(board[index][0], board[index][1], board[index][2], 2) {
+				sum += -3
+			}
+		}
+		results <- sum
+	}()
+
+	go func() {
+		sum := 0
+		for index := 0; index < 3; index++ {
+			if oneFromWin(board[0][index], board[1][index], board[2][index], 1) {
+				sum += 3
+			} else if oneFromWin(board[0][index], board[1][index], board[2][index], 2) {
+				sum -= 3
+			}
+		}
+		results <- sum
+	}()
+
+	go func() {
+		sum := 0
+
+		if oneFromWin(board[0][0], board[1][1], board[2][2], 1) {
+			sum += 3
+		} else if oneFromWin(board[0][0], board[1][1], board[2][2], 2) {
+			sum += -3
+		}
+		if oneFromWin(board[0][2], board[1][1], board[2][0], 1) {
+			sum += 3
+		} else if oneFromWin(board[0][2], board[1][1], board[2][0], 2) {
+			sum += -3
+		}
+		results <- sum
+	}()
+
+	total := 0
+	for values := 0; values < 3; values++ {
+		total += <-results
+	}
+
+	eval := getRowColFlattenBoard(boardNum)
+
+	choiceEvaluationBoard[eval[0]][eval[1]] = total + evaluateTiles(eval[0], eval[1], finishedBoard)
+}
+
+func evaluateTiles(evalRow int, evalCol int, miniBoard [3][3]int) int {
+	results := make(chan int, 3)
+
+	// Checks if diagonal is one from win
+	go func() {
+		sum := 0
+		// if is center tile or corner tile
+		if (evalRow == 1 && evalCol == 1) || (evalRow != 1 || evalCol != 1) {
+			diagonal := make([]int, 3)
+			// Checks if one of the diagonal tiles
+			if (evalRow == 0 && evalCol == 0) || (evalRow == 1 && evalCol == 1) || (evalRow == 2 && evalCol == 2) {
+				for index := 0; index < 3; index++ {
+					diagonal[index] = miniBoard[index][index]
+				}
+				if oneFromWin(diagonal[0], diagonal[1], diagonal[2], 1) {
+					sum += 10
+				} else if oneFromWin(diagonal[0], diagonal[1], diagonal[2], 2) {
+					sum += -10
+				}
+			}
+
+			// Checks if one of the other diagonal tiles
+			if (evalRow == 0 && evalCol == 2) || (evalRow == 1 && evalCol == 1) || (evalRow == 2 && evalCol == 0) {
+				counterIndex := 2
+				for index := 0; index < 3; index++ {
+					diagonal[index] = miniBoard[index][counterIndex]
+					counterIndex--
+				}
+				if oneFromWin(diagonal[0], diagonal[1], diagonal[2], 1) {
+					sum += 10
+				} else if oneFromWin(diagonal[0], diagonal[1], diagonal[2], 2) {
+					sum += -10
+				}
+			}
+		}
+		results <- sum
+	}()
+
+	// Checks if the row is one from win
+	go func() {
+		sum := 0
+		finRow := make([]int, 3)
+		for index := 0; index < 3; index++ {
+			finRow[index] = miniBoard[index][evalCol]
+		}
+		if oneFromWin(finRow[0], finRow[1], finRow[2], 1) {
+			sum += 10
+		} else if oneFromWin(finRow[0], finRow[1], finRow[2], 2) {
+			sum += -10
+		}
+		results <- sum
+	}()
+
+	// Checks if the column is one from win
+	go func() {
+		sum := 0
+		finCol := make([]int, 3)
+		for index := 0; index < 3; index++ {
+			finCol[index] = miniBoard[evalRow][index]
+		}
+		if oneFromWin(finCol[0], finCol[1], finCol[2], 1) {
+			sum += 10
+		} else if oneFromWin(finCol[0], finCol[1], finCol[2], 2) {
+			sum += -10
+		}
+		results <- sum
+	}()
+
+	total := 0
+	for values := 0; values < 3; values++ {
+		total += <-results
+	}
+
+	return total
+}
